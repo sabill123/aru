ffacf1b651b4e4b2bc8037af4c3c27ec2309ddf0
Jaeseok Han <jaeseokhan@letsur.ai>
2025-05-02 20:41:26 +0900

    Initial Commit

---
+++ aru/aruApp.swift
@@ -0,0 +1,17 @@
+//
+//  aruApp.swift
+//  aru
+//
+//  Created by Jaeseok Han on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct aruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aruTests/aruTests.swift
@@ -0,0 +1,17 @@
+//
+//  aruTests.swift
+//  aruTests
+//
+//  Created by Jaeseok Han on 5/2/25.
+//
+
+import Testing
+@testable import aru
+
+struct aruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
